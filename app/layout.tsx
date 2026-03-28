b278edf45f051eb7ce8a5339b85e277390467216
freedesk-cyb <omarconexion28@gmail.com>
2026-03-28 10:03:58 -0500

    fix: remove tailwind directives from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,3 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import "./globals.css";
